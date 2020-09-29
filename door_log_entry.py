ce8cd1a7d64967adb4b0f46f26f33c4b68e8c853
Judit Schmelko <j.schmelko@gmail.com>
2020-09-29 16:29:42 +0200

    from datetime import time

--- door_log_entry.py
+++ door_log_entry.py
@@ -1,10 +1,10 @@
+from datetime import time
 
 class DoorLogEntry:
     '''One log entry.'''
 
     def __init__(self, line):
         raw = line.split()
-        self.timehour = raw[0]
-        self.timeminute = raw[1]
+        self.hour_and_minute = time(int(raw[0]),int(raw[1]))
         self.person_id = int(raw[2])
         self.direction = raw[3]
