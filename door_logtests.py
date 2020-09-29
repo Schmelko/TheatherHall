c736d8263cb0d3e1fd8cafcc11c60c747502e052
Judit Schmelko <j.schmelko@gmail.com>
2020-09-29 15:48:40 +0200

    Add test_task1()

---
+++ door_logtests.py
@@ -0,0 +1,19 @@
+import unittest
+from door_log import DoorLog
+from door_log_entry import DoorLogEntry
+
+class TestSolution(unittest.TestCase):
+
+    def setUp(self):
+        with open('ajto.txt') as f:
+            lines = f.readlines()
+        
+        self.door_log = DoorLog(lines)
+
+def test_task1(self):
+        self.assertEqual(len(self.door_log.entries),516)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
