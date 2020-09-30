fc88ac9983d95e998f588af9874e20d7ece92926
Judit Schmelko <j.schmelko@gmail.com>
2020-09-30 15:04:23 +0200

    Add test_task2,2b

--- door_logtests.py
+++ door_logtests.py
@@ -12,9 +12,16 @@ class TestSolution(unittest.TestCase):
         
         self.door_log = DoorLog(lines)
 
-def test_task1(self):
-        self.assertEqual(len(self.door_log.entries),516)
+    def test_task1(self):
+        expected = 516
+        result = len(self.door_log.entries)
+        self.assertEqual(expected, result)
 
+    def test_task2(self):
+        self.assertEqual(self.door_log.find_first_entry_by_person_id_with_direction_in(), (2))
+
+    def test_task2b(self):
+        self.assertEqual(self.door_log.find_last_entry_by_person_id_with_direction_out(),)
 
 
 if __name__ == '__main__':
