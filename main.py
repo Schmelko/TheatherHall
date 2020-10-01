5719afa73e3e0946bb7f484ede6f7b2a61359307
Arnold Barna <arnold.barna@gmail.com>
2020-10-01 20:31:26 +0200

    door_log.find_no_of_crossings_by_person_ids()

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ print(door_log.find_first_entry_by_person_id_with_direction_in())
 
 print(door_log.find_last_entry_by_person_id_with_direction_out())
 
+print(door_log.find_no_of_crossings_by_person_ids())
