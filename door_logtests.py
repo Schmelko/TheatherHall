864bd906893b6b429f9ca09d4cf829678496ac2f
Arnold Barna <arnold.barna@gmail.com>
2020-10-02 18:57:26 +0200

    Add test_task3b()

--- door_logtests.py
+++ door_logtests.py
@@ -24,7 +24,6 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.door_log.find_last_entry_by_person_id_with_direction_out(),(6))
 
     def test_task3(self):
-
         expected = {
             1: 21, 2: 12, 3: 14, 4: 14, 5: 10, 6: 22, 7: 18, 9: 12, 10: 18, 11: 13, 12: 8, 14: 10, 15: 14,
             16: 16, 18: 10, 20: 10, 21: 16, 22: 9, 23: 22, 24: 15, 25: 32, 26: 10, 27: 14, 28: 10, 29: 13, 
@@ -34,6 +33,13 @@ class TestSolution(unittest.TestCase):
 
         self.assertEqual(expected, result)
 
+    def test_task3b(self):
+        expected = { 1: 21, 2: 12, 3: 14 }
+        result = self.door_log.find_no_of_crossings_by_person_ids()
+
+        self.assertEqual(expected[1], result[1])
+        self.assertEqual(expected[2], result[2])
+        self.assertEqual(expected[3], result[3])
 
 if __name__ == '__main__':
     unittest.main()
